6d6a9007a679a045702252329782b9ad5efb4a1f
Patrice Ferlet <metal3d@gmail.com>
2018-07-19 10:44:29 +0200

    Add SetTTL on GarbageCollector

--- app/providers/memorygc/main.go
+++ app/providers/memorygc/main.go
@@ -10,11 +10,13 @@ import (
 type MemoryGC struct {
 	ImageProvider providers.ImageProvider
 	mem           map[string]string
+	ttl           time.Duration
 }
 
 func NewMemoryGC() *MemoryGC {
 	mm := &MemoryGC{
 		mem: make(map[string]string),
+		ttl: 1 * time.Minute,
 	}
 
 	return mm
@@ -24,7 +26,7 @@ func (gc *MemoryGC) Collect(name string, data string) {
 
 	gc.mem[name] = data
 	go func(name, data string) {
-		time.Sleep(5 * time.Second)
+		time.Sleep(gc.ttl)
 		if _, ok := gc.mem[name]; ok {
 			log.Println("The image", name, "should be reinjected")
 			gc.ImageProvider.AddImage(name, data)
@@ -42,4 +44,8 @@ func (gc *MemoryGC) Delete(name string) error {
 
 func (gc *MemoryGC) SetImageProvider(p providers.ImageProvider) {
 	gc.ImageProvider = p
+}
+
+func (gc *MemoryGC) SetTTL(ttl time.Duration) {
+	gc.ttl = ttl
 }
